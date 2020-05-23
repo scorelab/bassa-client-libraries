90742167b389aca4cb03b7a4811c6d3cf688a884
K mehant <kmehant@gmail.com>
2020-05-23 19:50:52 +0530

    patch-2: Add retry and timeouts. Add File senders functions

---
+++ python_lib/errors.py
@@ -0,0 +1,15 @@
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+class InvalidUrl(Error):
+    """Raised for an invalid server URL"""
+    pass
+
+class IncompleteParams(Error):
+    """Raised for an incomplete inputs"""
+    pass
+
+class ResponseError(Error):
+    """Raised for an unsuccessful response from the server"""
+    pass

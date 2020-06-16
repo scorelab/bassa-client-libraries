d911f95f6a909cacf243f89d1afc57c3ecedb99a
K mehant <411843@student.nitandhra.ac.in>
2020-06-16 15:18:54 +0530

    Fix Travis

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 
-sudo chmod -R a+rwx $TRAVIS_BUILD_DIR
 docker-compose up &
 wait
-cd $TRAVIS_BUILD_DIR/python-lib
+cd $TRAVIS_BUILD_DIR/python_lib
 pip install -r requirements.txt
 python -m unittest test_bassa.py

9da3d2285fcfd02a8fe5b666faccab5440dc1197
K mehant <411843@student.nitandhra.ac.in>
2020-06-28 12:56:35 +0530

    Add setup and manifest
    Signed-off-by: K mehant <411843@student.nitandhra.ac.in>

--- python_lib/setup.py
+++ python_lib/setup.py
@@ -1,10 +1,30 @@
-from setuptools import setup
+import setuptools
+
 
 def readme():
     with open('README.md') as f:
         README = f.read()
     return README
 
-setup(
-    
+
+setuptools.setup(
+    name="bassa-kmehant",
+    version="1.0.0-alpha.1",
+    author="Mehant Kammakomati",
+    author_email="kmehant@scorelab.org",
+    description="Python Client Library for the Bassa Project",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/scorelab/BassaClient",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Apache 2.0 License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Libraries",
+    ],
+    python_requires='>=3.6',
 )
